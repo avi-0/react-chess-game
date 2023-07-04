e52a65e75b4320e7122b2a260cc92bb9e0dd4c77
avi <db0zmail@gmail.com>
2023-07-03 23:49:45 -0300

    hooked up chess.js

--- src/components/Chessboard/Chessboard.jsx
+++ src/components/Chessboard/Chessboard.jsx
@@ -1,11 +1,36 @@
 import Chessground from '@react-chess/chessground';
+import { useState } from 'react';
 
 import "chessground/assets/chessground.base.css";
 import "chessground/assets/chessground.brown.css";
 import "chessground/assets/chessground.cburnett.css";
 import "./Chessboard.css";
+import { Chess, SQUARES } from 'chess.js';
 
 export default function Chessboard() {
+    const [chess, setChess] = useState(new Chess());
+
+    const [moveSound, setMoveSound] = useState(new Audio("/sounds/move.mp3"));
+
+    function onMoved(orig, dest, meta) {
+        moveSound.play();
+
+        setChess((chess) => {
+            const newChess = new Chess(chess.fen());
+            newChess.move({from: orig, to: dest});
+            return newChess;
+        })
+    }
+
+    function getDests() {
+        const dests = new Map();
+        SQUARES.forEach(s => {
+          const ms = chess.moves({square: s, verbose: true});
+          if (ms.length) dests.set(s, ms.map(m => m.to));
+        });
+        return dests;
+      }
+
     return (
         <div className="chessboard-row-wrapper">
             <div className='chessboard-column-wrapper'>
@@ -21,6 +46,10 @@ export default function Chessboard() {
                             movable: {
                                 free: true,
                                 showDests: true,
+                                dests: getDests(),
+                                events: {
+                                    after: (orig, dest, meta) => onMoved(orig, dest, meta),
+                                },
                             }
                         }}
                     />
